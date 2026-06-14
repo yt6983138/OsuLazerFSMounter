7416b8a701034938f0b91c66bbf549489020490d
static_void <83499886+yt6983138@users.noreply.github.com>
2026-06-14 20:05:43 +0800

    Use submodule and make debugging process better

--- osu.Game.Rulesets.OsuVFSPlugin/Program.cs
+++ osu.Game.Rulesets.OsuVFSPlugin/Program.cs
@@ -1,8 +1,12 @@
-﻿namespace osu.Game.Rulesets.OsuVFSPlugin;
+﻿#if DEBUG
+namespace osu.Game.Rulesets.OsuVFSPlugin;
 
 internal class Program
 {
+	[STAThread]
 	private static void Main(string[] args)
 	{
+		osu.Desktop.Program.Main(args);
 	}
 }
+#endif
