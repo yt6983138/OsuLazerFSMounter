b2149f4789549da5800f74e71c26cf880649e61f
static_void <83499886+yt6983138@users.noreply.github.com>
2026-06-28 14:11:13 +0800

    Move model classes to here, and provide common abstraction

---
+++ OsuLazerFSMounter/RealmDataInfo.cs
@@ -0,0 +1,41 @@
+﻿using osu.Game.Beatmaps;
+using osu.Game.Models;
+using osu.Game.Skinning;
+using OsuLazerFSMounter.Utility;
+
+namespace OsuLazerFSMounter;
+
+public record struct RealmFileInfo(string Hash, string Path)
+{
+	public RealmFileInfo(RealmNamedFileUsage file)
+		: this(file.File.Hash, file.Filename) { }
+}
+
+public interface IOfflineRealmInfo
+{
+	Guid ID { get; }
+	RealmFileInfo[] Files { get; }
+
+	string GetDirectoryName();
+}
+public record class PartialSkinInfo(Guid ID, string Name, RealmFileInfo[] Files) : IOfflineRealmInfo
+{
+	public PartialSkinInfo(SkinInfo info)
+		: this(info.ID, info.Name, info.Files.ToRealmInfos().ToArray()) { }
+
+	public string GetDirectoryName()
+	{
+		return $"{Helper.SanitizeFileName(this.Name)} {this.ID.GetHashCode()}";
+	}
+}
+public record class PartialBeatmapSetInfo(string Title, Guid ID, int OnlineID, RealmFileInfo[] Files) : IOfflineRealmInfo
+{
+	public PartialBeatmapSetInfo(BeatmapSetInfo info)
+		: this(info.Metadata.Title, info.ID, info.OnlineID, info.Files.ToRealmInfos().ToArray()) { }
+
+	public string GetDirectoryName()
+	{
+		if (this.OnlineID <= 0) return $"{Helper.SanitizeFileName(this.Title)} {this.ID.GetHashCode()}";
+		else return $"{this.OnlineID} {Helper.SanitizeFileName(this.Title)}";
+	}
+}
