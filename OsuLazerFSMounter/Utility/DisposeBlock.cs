bcd038252d6963377966d3c7653aa90e6973a8ea
static_void <83499886+yt6983138@users.noreply.github.com>
2026-07-01 18:31:43 +0800

    Add utility to execute code when disposed

---
+++ OsuLazerFSMounter/Utility/DisposeBlock.cs
@@ -0,0 +1,19 @@
+﻿namespace OsuLazerFSMounter.Utility;
+public class DisposeBlock : IDisposable
+{
+	private volatile int _hasDisposed = 0;
+
+	public bool HasDisposed => this._hasDisposed != 0;
+	public Action Disposer { get; set; }
+
+	public DisposeBlock(Action disposer)
+	{
+		this.Disposer = disposer;
+	}
+
+	public void Dispose()
+	{
+		if (Interlocked.Exchange(ref this._hasDisposed, 1) == 0)
+			this.Disposer.Invoke();
+	}
+}
